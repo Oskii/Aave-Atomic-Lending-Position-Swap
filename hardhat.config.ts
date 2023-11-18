611792933204f49a1570426a92e0989fef3601da
oskiiforte <ochambers@forte.io>
2023-11-18 23:26:55 +0000

    add deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
@@ -6,16 +8,22 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.20",
+	typechain: {
+		outDir: "typechain", // Directory for generated types
+		target: "ethers-v5",
+	},
 	networks: {
 		// Example of Ethereum Mainnet
 		mainnet: {
 			url: process.env.MUMBAI_RPC_URL,
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+			accounts:
+				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
 		},
 		// Example of Rinkeby Testnet
 		rinkeby: {
 			url: process.env.SEPOLIA_RPC_URL,
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+			accounts:
+				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
 		},
 	},
 };
